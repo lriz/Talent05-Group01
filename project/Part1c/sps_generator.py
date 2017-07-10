ec351340beda60b6236b107b6fe164330f0c3306
Noam <noam.gavrielov@mail.huji.ac.il>
2017-07-10 21:29:20 +0200

    Fixed the calc_m_scheme_basis function and unit tested the s-d-shell:     - for N=2 particles there are 14 states with M=0.     - for N=3 particles there are 37 states with M=1/2.     - for N=4 particles there are 81 states with M=0.

--- project/Part1c/sps_generator.py
+++ project/Part1c/sps_generator.py
@@ -13,14 +13,14 @@ class sps_generator(object):
 
     def calc_m_broken_basis(self, shell_configurations_list):
         sps_index = 1
-        for p_level_index in self.input_dict["shell-configuration P-levels"].keys():
-            level_name = self.input_dict["shell-configuration P-levels"][p_level_index]["name"]
+        for p_level_index in self.input_dict["shell-orbit P-levels"].keys():
+            level_name = self.input_dict["shell-orbit P-levels"][p_level_index]["name"]
             N_number = [shell['N'] for shell in  shell_configurations_list  if shell['name'] == level_name][-1] #TODO: check that not empty.
-            j_total = self.input_dict["shell-configuration P-levels"][p_level_index]["2J-total"]
+            j_total = self.input_dict["shell-orbit P-levels"][p_level_index]["2J-total"]
             for m_j in range(-j_total, j_total+1, 2):
                 self.all_sps_list.append(single_particle_state(p_level_index,
                                                              N_number,
-                                                             self.input_dict["shell-configuration P-levels"][p_level_index]["angular momentum"],
+                                                             self.input_dict["shell-orbit P-levels"][p_level_index]["angular momentum"],
                                                              j_total,
                                                              m_j,
                                                              sps_index))
@@ -52,21 +52,7 @@ class sps_generator(object):
 
     def calc_m_scheme_basis(self, m_broken_basis):
         for sps in m_broken_basis:
-            sps_temp_dict = OrderedDict()
-            for p in self.input_dict["shell-configuration P-levels"]:
-                sps_p_list = []
-                for i in sps:
-                    if i.get_p() == p:
-                        sps_p_list.append(i)
-                sps_temp_dict[p] = sps_p_list
-            sps_temp_dict = OrderedDict((k, v) for k, v in sps_temp_dict.iteritems() if v)
-            m_bool = True
-            for p, value in sps_temp_dict.iteritems():
-                if sum([v.get_m_j() for v in value]) == self.input_dict["shell-configuration P-levels"][p]["M-total"]:
-                    continue
-                else:
-                    m_bool = False
-            if m_bool:
+            if sum([i.get_m_j() for i in sps]) in self.input_dict["2M-total"]:
                 self.m_scheme_basis.append(sps)
 
     def get_m_scheme_basis(self):
@@ -81,12 +67,12 @@ class sps_generator(object):
 
     def print_m_scheme_basis(self):
         print
-        m_total_list = [(self.input_dict["shell-configuration P-levels"][p]["M-total"], self.input_dict["shell-configuration P-levels"][p]["name"]) for p in self.input_dict["shell-configuration P-levels"]]
+        m_total_list = [(self.input_dict["2M-total"], self.input_dict["shell-orbit P-levels"][p]["name"]) for p in self.input_dict["shell-orbit P-levels"]]
         m_total_list = []
-        for p in self.input_dict["shell-configuration P-levels"]:
-            m_total_list.append(self.input_dict["shell-configuration P-levels"][p]["M-total"])
-            m_total_list.append("".join(("(",self.input_dict["shell-configuration P-levels"][p]["name"],")")))
-        print "".join(("sps with M-total = ","{} "*len(m_total_list))).format(*m_total_list)
+        for p in self.input_dict["shell-orbit P-levels"]:
+            m_total_list.append(self.input_dict["2M-total"])
+            m_total_list.append("".join(("(",self.input_dict["shell-orbit P-levels"][p]["name"],")")))
+        print "".join(("sps with 2M-total = ","{} "*len(m_total_list))).format(*m_total_list)
         print 'Basis length:', len(self.m_scheme_basis)
         print self.m_scheme_basis
 
