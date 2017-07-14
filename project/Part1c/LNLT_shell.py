54c9d7a96b713f6a218bfce3668dd17df3eea8a5
Noam <noam.gavrielov@mail.huji.ac.il>
2017-07-14 13:51:03 +0200

    updates

--- project/Part1c/LNLT_shell.py
+++ project/Part1c/LNLT_shell.py
@@ -5,12 +5,13 @@ import argparse
 from collections import OrderedDict
 from single_particle_state_class import single_particle_state
 from hamiltonian_unperturbed import hamiltonian_unperturbed
-from hamiltonian_unperturbed import hamiltonian_unperturbed_pairing
+from hamiltonian_j_squared import hamiltonian_j_squared
 from sps_generator import sps_generator
 from interaction_hamiltonian import TwoBodyInteraction
 from pairing_potential import PairingPotential
 from general_potential import GeneralHamiltonian
 from generate_many_body_basis import generate_many_body_basis
+
 
 def shell_configurations():
     return [{'name': '0s1/2', 'N': 0},
@@ -28,35 +29,42 @@ def shell_configurations():
 #################### Argparse ####################
 # get arguments from the command line.
 parser = argparse.ArgumentParser(description='None')  # Generate a parser.
-parser.add_argument('-f','--file',help='a json input file name for the program', type=str,required=True)  # Define a command line operation.
+#parser.add_argument('-c','--config',help='a json config file name for the program', type=str,required=True)  # Define a command line operation.
+#group = parser.add_mutually_exclusive_group(required=True)
+# Only one of these can appear in the terminal (mutually exclusive)
+#group.add_argument('-f','--file',help='a json input file name for the program', type=str)  # a command line operation.
+#group.add_argument('-i','--interaction_file',help='a text input file name to a given interaction file', type=str)  # a command line operation.
+parser.add_argument('-f','--file',help='a json input file name for the program', type=str)  # a command line operation.
 args = parser.parse_args()
 #################### Argparse ####################
 
 folder_name = 'input_files/'  # Folder of input files.
 with open("".join((folder_name,args.file))) as data_file:
-        input_dict = json.load(data_file)  # A dictionary. Keys are the name of input, values are the value of input.
+        orbits_dict = json.load(data_file)  # A dictionary. Keys are the name of input, values are the value of input.
+
+#with open("".join((folder_name,args.config))) as data_file:
+#        config_dict = json.load(data_file)  # A dictionary. Keys are the name of input, values are the value of input.
 
 #################### Global parameters ####################
 shell_configurations_list = shell_configurations()
 # Ordering the dictionary according to P values.
-input_dict["shell-orbit P-levels"] = OrderedDict(sorted(input_dict["shell-orbit P-levels"].iteritems(), key=lambda x: x[0]))
+orbits_dict["shell-orbit P-levels"] = OrderedDict(sorted(orbits_dict["shell-orbit P-levels"].iteritems(), key=lambda x: x[0]))
 #################### Global parameters ####################
 
 #################### Get m_scheme_basis ####################
-sps_generator_obj = sps_generator(input_dict)
+sps_generator_obj = sps_generator(orbits_dict)
 sps_generator_obj.calc_m_broken_basis(shell_configurations_list)
 m_broken_basis = sps_generator_obj.get_m_broken_basis()
-get_all_sps_list = sps_generator_obj.get_all_sps_list()
+sps_list = sps_generator_obj.get_sps_list()
 sps_generator_obj.calc_m_scheme_basis(m_broken_basis)
 m_scheme_basis = np.array(sps_generator_obj.get_m_scheme_basis())
-
 #################### Get m_scheme_basis ####################
 
 #################### Print ####################
-print(get_all_sps_list)
+print(sps_list)
 sps_generator_obj.print_sps()
 print
-print "Number of general {}-particle states:".format(input_dict["number of particles"]),len(m_broken_basis)
+print "Number of general {}-particle states:".format(orbits_dict["number of particles"]),len(m_broken_basis)
 sps_generator_obj.print_m_scheme_basis()
 print hamiltonian_unperturbed_pairing(m_scheme_basis)
 #################### Print ####################
@@ -85,9 +93,18 @@ H = H0+HI
     #print("Hamiltonian:")
     #print H
 
-eigs,vecs = np.linalg.eig(np.array(H))
+energies, eig_vectors_list = np.linalg.eig(np.array(H))
 print("The eigen values:")
-print(np.sort(eigs))
+print(np.sort(energies))
+#np.set_printoptions(threshold='nan')  # Command to print entire matrix.
+H_j = hamiltonian_j_squared(eig_vectors_list, m_scheme_basis)
+# Count num. of off-diagonal elements (search for degeneracies)
+# Remove the diagonal part of H_j and then count non-zero elements.
+print np.count_nonzero(H_j - np.diag(np.diagonal(H_j)))
+print np.diagonal(H_j)  # Look at the diagonal part of H_j.
+for j in np.diagonal(H_j):
+        print j, np.roots([1,1,-j])[-1]
+#np.count_nonzero(h_j - np.diag(np.diagonal(h_j)))
 #    energies.append(np.sort(eigs))
 
 #energies = np.array(energies)
