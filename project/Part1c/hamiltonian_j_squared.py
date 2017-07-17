8b7783df1d6dc01800b3c67ed5b08d879013611d
Noam <noam.gavrielov@mail.huji.ac.il>
2017-07-17 14:48:25 +0200

    no imporatnt changes

--- project/Part1c/hamiltonian_j_squared.py
+++ project/Part1c/hamiltonian_j_squared.py
@@ -12,7 +12,7 @@ def hamiltonian_j_squared(eig_vectors_list, m_scheme_basis):
     j_state = np.array([])  # j(j+1) calculated for each eig in m_scheme_basis.
     # Calculate the j(j+1) value for each m_scheme_basis state and store it in the corresponding index.
     for m_state in m_scheme_basis:
-        j_state = np.append(j_state, np.sum([l.get_j()*(l.get_j()+1) for l in m_state]))
+        j_state = np.append(j_state, np.sum([k.get_j()/2*(k.get_j()/2+1) for k in m_state]))
     for i, eig_i in enumerate(eig_vectors_list):
         eig_mult_j = np.multiply(eig_i, j_state)  # Multiply corresponding components of j_state with eig_i - returns a new np.array (not a dot product).
         for j, eig_j in enumerate(eig_vectors_list):
