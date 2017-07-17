1d3b7be823dd606fbd6bdc93849b80bbeb72d9de
Noam <noam.gavrielov@mail.huji.ac.il>
2017-07-17 15:07:41 +0200

    deleted J^2 function

--- project/Part1c/garab_main.py
+++ project/Part1c/garab_main.py
@@ -11,33 +11,10 @@ from interaction_hamiltonian import TwoBodyInteraction
 from pairing_potential import PairingPotential
 from general_potential import GeneralHamiltonian
 from generate_many_body_basis import generate_many_body_basis
-from j_operator import get_JJ_operator
 from level_ploter import LevelPloter
 
 A = 20
 n = A-16
-
-
-def hamiltonian_j_squared(eig_vectors_list, m_scheme_basis):
-    """
-    The j(j+1) Hamiltonian.
-    :param m_scheme_basis: a basis of sps in m_scheme.
-    :param eig_vectors_list: a list of all eigenvectors of the diagonalized matrix.
-    :return: the Hamiltonian matrix.
-    """
-    #TODO: check len(m_scheme_basis) == len(eig_vectors)
-    dim = len(eig_vectors_list)
-    j_squared_matrix = np.zeros((dim, dim))  # the unperturbed  Hamiltonian matrix.
-    j_state = np.array([])  # j(j+1) calculated for each eig in m_scheme_basis.
-    # Calculate the j(j+1) value for each m_scheme_basis state and store it in the corresponding index.
-    for m_state in m_scheme_basis:
-        j_state = np.append(j_state, np.sum([l.get_j()*(l.get_j()+1) for l in m_state]))
-    for i, eig_i in enumerate(eig_vectors_list):
-        eig_mult_j = np.multiply(eig_i, j_state)  # Multiply corresponding components of j_state with eig_i - returns a new np.array (not a dot product).
-        for j, eig_j in enumerate(eig_vectors_list):
-            dot_prod = np.dot(eig_mult_j, eig_j)
-            j_squared_matrix[i,j] = dot_prod
-    return j_squared_matrix
 
 # Read the potential
 V = GeneralHamiltonian("sdshellint.dat")
@@ -69,10 +46,6 @@ print(np.sort(eigs))
 
 level_diagram = LevelPloter(np.sort(eigs))
 level_diagram.plotLevels()
-
-
-jjmat =  get_JJ_operator(V.get_sp_energies(),mp_basis_m0)
-print jjmat
 
 values=[]
 for i,e in enumerate(eigs):
