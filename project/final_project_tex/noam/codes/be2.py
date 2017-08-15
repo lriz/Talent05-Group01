03caa2f73cd0c7a17c199800f4e1fd968f7b0cdc
noamgavri <noam.gavrielov@mail.huji.ac.il>
2017-08-15 11:59:29 +0300

    Added USDA and CCEI

--- project/final_project_tex/noam/codes/be2.py
+++ project/final_project_tex/noam/codes/be2.py
@@ -6,14 +6,21 @@ def main():
     A program that plots B(E2)'s between different init. and fin. states of the Oxygen-18-26 isotopes as a function
     of nucleon (neutron) number A (N)
     """
-    twop_zerop_theo = [3.263,4.117,3.968,1.666,2.190]  # Single-particle units.
-    twop_zerop_exp = [3.32,1.80,1.2]
-    error_exp = [0.09,0.07,0.5]
     A = [18,20,22,24,26]
-    twop_zerop_theo_weis = [x/weisskopf_units(2,a) for x,a in zip(twop_zerop_theo,A)]  # Weisskopf units.
-    plt.plot(A,twop_zerop_theo_weis,'o-',color='blue',label='USDB')
-    plt.plot(A[:3],twop_zerop_exp,'o-',color='orange',label='Exp.')
-    plt.errorbar(A[:3],twop_zerop_exp,yerr=error_exp,color='orange',capsize=5)
+    twop_zerop_usda = [3.069,3.950,3.765,1.845,2.066]  # Single-particle units.
+    twop_zerop_usda_weis = [x/weisskopf_units(2,a) for x,a in zip(twop_zerop_usda,A)]  # Weisskopf units.
+    twop_zerop_usdb = [3.263,4.117,3.968,1.666,2.190]  # Single-particle units.
+    twop_zerop_usdb_weis = [x/weisskopf_units(2,a) for x,a in zip(twop_zerop_usdb,A)]  # Weisskopf units.
+    twop_zerop_ccei = [3.499,4.261,4.228,1.643,2.550]  # Single-particle units.
+    twop_zerop_ccei_weis = [x/weisskopf_units(2,a) for x,a in zip(twop_zerop_ccei,A)]  # Weisskopf units.
+    twop_zerop_exp = [3.32,1.80,1.2]  # W.u.
+    error_exp = [0.09,0.07,0.5]
+
+    plt.plot(A,twop_zerop_usda_weis,'o-',color='red',label='USDA')
+    plt.plot(A,twop_zerop_usdb_weis,'o-',color='blue',label='USDB')
+    plt.plot(A,twop_zerop_ccei_weis,'o-',color='orange',label='CCEI')
+    plt.plot(A[:3],twop_zerop_exp,'o-',color='black',label='Exp.')
+    plt.errorbar(A[:3],twop_zerop_exp,yerr=error_exp,color='black',capsize=5)
     #plt.title('Oxygen isotopes')
     plt.xlabel('$A$',fontsize=20)
     plt.ylabel('$B(E2;2_1^+ \\rightarrow 0_1^+)$(W.u.)',fontsize=20)
